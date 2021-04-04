48f4fcf625f112ab115d94f849af3acab4ee0a85
unknown <80460125+Shathur@users.noreply.github.com>
2021-04-04 12:01:33 +0300

    Added TimeSeriesSplitGroups

--- cv_splits.py
+++ cv_splits.py
@@ -22,3 +22,33 @@ class Random_Splits(_BaseKFold):
         for i in range(self.n_splits):
             yield (indices[groups.isin(group_lst[i*cutoff_eras: i*cutoff_eras + cutoff_eras])],
                    indices[groups.isin(group_lst[i*cutoff_eras: i*cutoff_eras + cutoff_eras])])
+
+
+class TimeSeriesSplitGroups(_BaseKFold):
+    """
+    Code kindly provided by Michael Oliver in the Numer.ai forum
+    https://forum.numer.ai/t/era-wise-time-series-cross-validation/791
+    """
+    def __init__(self, n_splits=None):
+        super().__init__(n_splits, shuffle=False, random_state=None)
+
+    def split(self, X, y=None, groups=None):
+        X, y, groups = indexable(X, y, groups)
+        n_samples = _num_samples(X)
+        n_splits = self.n_splits
+        n_folds = n_splits + 1
+        group_list = np.unique(groups)
+        n_groups = len(group_list)
+        if n_folds > n_groups:
+            raise ValueError(
+                ("Cannot have number of folds ={0} greater"
+                 " than the number of samples: {1}.").format(n_folds,
+                                                             n_groups))
+        indices = np.arange(n_samples)
+        test_size = (n_groups // n_folds)
+        test_starts = range(test_size + n_groups % n_folds,
+                            n_groups, test_size)
+        test_starts = list(test_starts)[::-1]
+        for test_start in test_starts:
+            yield (indices[groups.isin(group_list[:test_start])],
+                   indices[groups.isin(group_list[test_start:test_start + test_size])])
