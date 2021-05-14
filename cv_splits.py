16323c23bf25ff024f37ce2d611605a21651c7a2
unknown <80460125+Shathur@users.noreply.github.com>
2021-05-14 12:38:36 +0300

    Added PurgedKFold and cvscore from Advances Prado

--- cv_splits.py
+++ cv_splits.py
@@ -2,6 +2,8 @@ import numpy as np
 
 from sklearn.model_selection._split import _BaseKFold, indexable, _num_samples
 
+from sklearn.metrics import log_loss, accuracy_score
+
 
 class Random_Splits(_BaseKFold):
     def __init__(self, n_splits=None):
@@ -52,3 +54,63 @@ class TimeSeriesSplitGroups(_BaseKFold):
         for test_start in test_starts:
             yield (indices[groups.isin(group_list[:test_start])],
                    indices[groups.isin(group_list[test_start:test_start + test_size])])
+
+
+class PurgedKfold(_BaseKFold):
+    """
+    Extend KFold class to work with labels that span intervals
+    The train is purged of observations overlapping test-label intervals
+    Test set is assumed contiguous (shuffle=False), w/o training samples in
+
+    Code was adapted from Advances in Financial ML by Marcos Lopez De Prado.
+    Snippet 7.3
+
+    t1 : must be a pd.Series
+    X and t1 must have the same index values
+    """
+    def __init(self, n_splits=None, t1=None, pctEmbargo=None):
+        super(PurgedKfold, self).__init(n_splits, shuffle=False, random_state=None)
+        self.t1=t1
+        self.pctEmbargo = pctEmbargo
+
+    def split(self, X, y=None, groups=None):
+        indices=np.arange(X.shape[0])
+        mbrg=int(X.shape[0].self.pctEmbargo)
+        test_starts = [(i[0], i[-1]+1) for i in np.array_split(np.arange(X.shape[0]), self.n_splits)]
+
+        for i, j in test_starts:
+            t0 = self.t1.index[i]
+            test_indices = indices[i: j]
+            maxt1idx = self.t1.index.searchsorted(self.t1[test_indices].max())
+            train_indices = self.t1.index.searchsorted(self.t1[self.t1<=t0].index)
+            if maxt1idx<X.shape[0]:
+                train_indices = np.concatenate((train_indices, indices[maxt1idx+mbrg:]))
+            yield train_indices, test_indices
+
+
+def cvscore(clf, X, y, sample_weight, scoring='neg_log_loss', t1=None, cv=None, cvGen=None, pctEmbargo=None):
+    """
+    Alternative to cross_val_score for the PurgedKFold class
+    cross_val_score will give different results because it passes weights to the fit method, but
+    not to the log_loss method.
+
+    scoring : Must belong in ['neg_log_loss', 'accuracy']
+    """
+    if cvGen is None:
+        cvGen = PurgedKfold(n_splits=cv, t1=t1, pctEmbargo=pctEmbargo)
+    score = []
+
+    for train, test in cvGen.split(X=X):
+        fit=clf.fit(X=X.iloc[train, :], y=y.iloc[train], sample_weight=sample_weight.iloc[train].values)
+        if scoring == 'neg_log_loss':
+            prob=fit.predict_proba(X.iloc[test, :])
+            score_ = -log_loss(y.iloc[test], prob, sample_weight=sample_weight.iloc[test].values, labels=clf.classes_)
+        else:
+            pred = fit.predict(X.iloc[test, :])
+            score_ = accuracy_score(y.iloc[test], pred, sample_weight=sample_weight.iloc[test].values)
+
+        score.append(score_)
+
+    return np.array(score)
+
+
