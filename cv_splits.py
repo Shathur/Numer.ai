29a561798e5ea9dafc3aef39ffb7a6bd35b78ec3
Shathur <iliaslangas@gmail.com>
2021-12-06 13:34:06 +0200

    group_list in TimeSeriesSplitGroups tolist()

--- cv_splits.py
+++ cv_splits.py
@@ -44,7 +44,7 @@ class TimeSeriesSplitGroups(_BaseKFold):
         n_samples = _num_samples(X)
         n_splits = self.n_splits
         n_folds = n_splits + 1
-        group_list = np.unique(groups)
+        group_list = np.unique(groups).tolist()
         n_groups = len(group_list)
         if n_folds > n_groups:
             raise ValueError(
