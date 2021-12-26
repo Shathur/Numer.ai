f25389291b4b6a99d0811f69b049d04c9d00ff2b
Shathur <iliaslangas@gmail.com>
2021-12-26 12:37:16 +0200

    cv_split_creator added multiple classes functionality

--- cv_splits.py
+++ cv_splits.py
@@ -285,7 +285,8 @@ def cross_validate_train(feature_names, cv_split_data, target_name=TARGET_NAME, 
     return val_correlations, tour_correlations, val_sharpe_cv, tour_sharpe_cv
 
 
-def cv_split_creator(df, col, n_splits=4, is_string=False):
+def cv_split_creator(df, col, cv_scheme='TimeSeriesSplitGroups', n_splits=4, is_string=False, extra_params={}):
+
     # add another column with date id to feed the cv splitter
     if col+'_No' not in df.columns:
         if is_string:
@@ -301,7 +302,7 @@ def cv_split_creator(df, col, n_splits=4, is_string=False):
         dateno_values = df[col+'_No']
 
     # create TimeSeriesGroupSplit object and use .split to create our folds
-    time_group_splitter = TimeSeriesSplitGroups(n_splits=n_splits).split(df, groups=dateno_values)
+    time_group_splitter = cv_scheme(n_splits=n_splits).split(df, groups=dateno_values, **extra_params)
 
     # keep the data in list format
     cv_split_data = list(time_group_splitter)
