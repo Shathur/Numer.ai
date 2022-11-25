b0ddf950185ac71196380a00ad064cff913ae630
Shathur <iliaslangas@gmail.com>
2022-11-25 13:56:31 +0200

    dependencies fix

--- cv_splits.py
+++ cv_splits.py
@@ -5,6 +5,7 @@ from sklearn.model_selection._split import _BaseKFold, indexable, _num_samples
 from sklearn.metrics import log_loss, accuracy_score
 
 from Numerai import utils
+from Numerai import models
 from Numerai.predictions import predict_in_era_batch
 
 from Numerai.NewData.utils_new import *
@@ -243,7 +244,7 @@ def cross_validate_train(feature_names, cv_split_data, target_name=TARGET_NAME, 
 
         train_tuple = [X_train, y_train]
         val_tuple = [X_val, y_val]
-        model = utils.run_model(train_data=train_tuple, val_data=val_tuple, model_type=type_of_model,
+        model = models.run_model(train_data=train_tuple, val_data=val_tuple, model_type=type_of_model,
                                 model_params=model_params, save_to_drive=save_to_drive,
                                 save_folder=save_folder, cv_count=cv_count)
 
