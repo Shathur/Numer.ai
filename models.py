1f2f2937fa1ed7c78d3dcebdbf1a69ea53929119
Shathur <iliaslangas@gmail.com>
2023-01-13 15:47:08 +0200

    updated models.py

--- models.py
+++ models.py
@@ -33,35 +33,65 @@ def get_model_lst(num_models=1, prefix=None, folder_name=None, verbose=True):
     return model_lst_final
 
 
-def run_model(train_data=None, val_data=None, model_type='xgb', model_params=None, save_to_drive=False,
-              save_folder=None, cv_count=None):
+def run_model(train_data=None, val_data=None, model_type='xgb', task_type='regression', model_params=None,
+              fit_params=None, save_to_drive=False, save_folder=None, legacy_save=True, cv_count=None):
     X_train, y_train = train_data
     X_val, y_val = val_data
 
-    model = create_model(model_type=model_type, model_params=model_params)
+    model = create_model(model_type=model_type, task_type=task_type, model_params=model_params)
 
-    model.fit(X_train, y_train, eval_set=[(X_val, y_val)], early_stopping_rounds=10, verbose=False)
+    if fit_params is None:
+        fit_params = {
+            'early_stopping_rounds': 10,
+            'verbose': False
+        }
+    else:
+        pass
+
+    model.fit(X_train, y_train, eval_set=[(X_val, y_val)], **fit_params)
+
+    # if we are using lgb we first need to keep the booster of our model
+    if model_type == 'lgb':
+        model = model.booster_
 
     if save_to_drive:
-        model.save_model(os.path.join(save_folder, f'model_{cv_count}.{model_type}'))
-    else:
-        model.save_model(os.path.join(f'model_{cv_count}.{model_type}'))
+        if legacy_save:
+            if model_type=='lgb':
+                model.save_model(os.path.join(save_folder,f'model_{cv_count}.{model_type}'))
+            else:
+                model.save_model(save_folder + 'model_{}.'.format(cv_count)+model_type)
+        else:
+            if model_type=='lgb':
+                model_json = model.dump_model()
+                with open(os.path.join(save_folder,f'model_{cv_count}.{model_type}'),'w') as f:
+                    json.dump(model_json, f)
+                model.save_model(os.path.join(save_folder,f'model_{cv_count}.{model_type}'))
+            else:
+                model.save_model(save_folder + 'model_{}.json'.format(cv_count))
+     # else:
+     #     if legacy_save:
+     #         model.save_model('model_{}.'.format(cv_count)+model_type)
+     #     else:
+     #         model.save_model('model_{}.json'.format(cv_count))
 
     return model
 
 
-def create_model(model_type='xgb', model_params=None):
+def create_model(model_type='xgb', task_type='regression', model_params=None):
     if model_params is None:
         model_params = get_default_params(model_type=model_type)
     else:
         pass
-
     if model_type == 'lgb':
         model = lgb.LGBMRegressor()
         model.set_params(**model_params)
 
     if model_type == 'xgb':
-        model = xgb.XGBRegressor()
+        model = XGBRegressor()
+        model.set_params(**model_params)
+
+    if task_type == 'classification':
+        model = XGBClassifier()
         model.set_params(**model_params)
 
     return model
